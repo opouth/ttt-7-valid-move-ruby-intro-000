42f19563ceae5e2ef30e4b6ba051b40ca9737613
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 19:17:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,21 +1,25 @@
-board = [" "," "," "," "," "," "," "," "," "]
-puts "Enter a position on the board 1-9:"
-user_input = gets.chomp
-index = user_input.to_i - 1
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
 
-def valid_move?(index, board)
-  index.between?(0, 8) && !(position_taken?(board, index))
-end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-
-def position_taken?(board, index)
-  #taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    return false
-  else
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
-   end
+  else
+    return false
+  end
+
+
 end
-
-
-valid_move?(index, board)
