b1ed24b771112715a35dbf59387fad3475aa9790
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 18:04:31 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,6 @@ choosen_num = gets.chomp
 
 def valid_move?(board, index)
   choosen_num.to_i.between?(1, 9) && !(position_taken(board, choosen_num))
-  return true
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
