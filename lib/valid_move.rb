4c57aaefb7569c82d7a0f71bbaff1bac65ba1ef1
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 18:04:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,10 +7,9 @@ def valid_move?(board, index)
   choosen_num.to_i.between?(1, 9) && !(position_taken(board, choosen_num))
   return true
 end
-valid_move?(board, index)
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
  return true
 end
-position_taken?(board, index)
