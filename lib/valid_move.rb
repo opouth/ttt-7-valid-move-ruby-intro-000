bf815661776f2d1e0973b61365f4a6b8fff2cff8
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 17:26:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ puts "Pls choose a position between 1 & 9"
 choosen_num = gets.strip
 
 def valid_move?(board, index)
-  choosen_num.between?(1, 9) && choosen_num != position_taken(board, choosen_num)
+  choosen_num.between?(1, 9) && choosen_num.to_i != position_taken(board, choosen_num)
   return true 
 end
 
