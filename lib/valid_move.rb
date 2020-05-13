59892aea2556a046c697768fc9850700e6133fab
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 23:07:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,26 +1,26 @@
-board = [" "," "," "," "," "," "," "," "," "]
-puts "Enter a position on the board 1-9:"
-user_input = gets.chomp
-index = user_input.to_i - 1
-
 def valid_move?(board, index)
-  if position_taken? == true
+  def position_taken?(board, index)
+    #taken = nil
+    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+      return false
+    else
+      return true
+     end
+  end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
-  if index.between?(0, 8) && !(position_taken?(board, index)) == true
-    return true
-end
 
-
-def position_taken?(board, index)
-  #taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    return false
-  else
-    return true
-   end
 end
-end
-valid_move?(board, index)
