538b55d6c2c1f7bfff02b58805dff29d2f326f58
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 18:05:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,5 +9,10 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
- !(board[index] == " " || board[index] == "" || board[index] == nil)
+  #taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+   end
 end
