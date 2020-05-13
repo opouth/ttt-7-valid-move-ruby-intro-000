79973960933fe76755e8c20e10468b11fb0e8b0e
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 18:06:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 puts "Pls choose a position between 1 & 9"
-choosen_num = gets.chomp
+choosen_num = gets.chomp.to_i
 
 def valid_move?(board, index)
-  choosen_num.to_i.between?(1, 9) && !(position_taken(board, choosen_num))
+  choosen_num.between?(1, 9) && !(position_taken(board, choosen_num))
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
